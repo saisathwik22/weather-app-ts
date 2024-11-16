767689c7e15e838e2a685f3b317785da4d211ffa
saisathwik22 <reddysaisathwik022@gmail.com>
2024-11-16 09:28:57 +0530

    API endpoints added CodeStage - 52:00

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,5 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { useGeolocation } from "@/hooks/use-geolocation";
+import { RefreshCw } from "lucide-react";
+
 const WeatherDashboard = () => {
-  return <div>weatherdashboard</div>;
+  const {
+    coordinates,
+    error: locationError,
+    getLocation,
+    isLoading: locationLoading,
+  } = useGeolocation();
+  console.log(coordinates);
+
+  const handleRefresh = () => {
+    getLocation();
+    if (coordinates) {
+      // reload weather data
+    }
+  };
+
+  if (locationLoading) {
+  }
+
+  return (
+    <div className='space-y-4'>
+      {/* Favorite Cities */}
+      <div className='flex items-center justify-between'>
+        <h1 className='text-xl font-bold tracking-tight'>My Location</h1>
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          // onClick={handleRefresh}
+          // disabled={}
+        >
+          <RefreshCw className='h-4 w-4' />
+        </Button>
+      </div>
+      {/* Current and Hourly weather */}
+    </div>
+  );
 };
 
 export default WeatherDashboard;
