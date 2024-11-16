006045e8e8d58fa3a29f0aae6f082d19f334964b
saisathwik22 <reddysaisathwik022@gmail.com>
2024-11-16 10:50:19 +0530

    Current Loc fetched, API endpoints integrated. CodeStage - 56:00

--- src/components/loading-skeleton.tsx
+++ src/components/loading-skeleton.tsx
@@ -3,6 +3,7 @@ import { Skeleton } from "./ui/skeleton";
 function WeatherSkeleton() {
   return (
     <div className='space-y-6'>
+      
       <div className='grid gap-6'>
         <Skeleton className='h-[300px] w-full rounded-lg' />
         <Skeleton className='h-[300px] w-full rounded-lg' />
