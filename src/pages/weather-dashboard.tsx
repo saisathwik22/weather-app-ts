6845a8b3d30f63aa8fc08d54aa0ae601de324dfb
saisathwik22 <reddysaisathwik022@gmail.com>
2024-11-23 09:14:49 +0530

    Weather Details added CodeStage - 1:36:00

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -3,6 +3,7 @@ import HourlyTemperature from "@/components/hourly-temperature";
 import WeatherSkeleton from "@/components/loading-skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
+import WeatherDetails from "@/components/weather-details";
 import { useGeolocation } from "@/hooks/use-geolocation";
 import {
   useForecastQuery,
@@ -118,11 +119,10 @@ const WeatherDashboard = () => {
           />
 
           <HourlyTemperature data={forecastQuery.data} />
-
-          {/* hourly temperature */}
         </div>
         <div>
           {/* details */}
+          <WeatherDetails data={weatherQuery.data} />
           {/* forecast */}
         </div>
       </div>
